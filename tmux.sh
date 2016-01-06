3840fb2a65aa4a1034e8c5052f21fd2ec0f30507
edgar.klerks <edgar.klerks@sanoma.com>
2016-01-06 23:03:10 +0100

    initial commit

---
+++ tmux.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+export TOPIC="${1-master}"
+export DIR="$2"
+
+if [[ -z "$TOPIC" ]]; then 
+        export TOPIC="master"
+fi 
+
+SES_NAME="$(openssl rand -hex 3)"
+CLIENTS=$(tmux list-clients)
+echo "$CLIENTS"
+
+if [[ -z "$CLIENTS" ]]; then
+   tmux attach -t "master" || tmux new-session -s "master" \; set-environment TOPIC "$TOPIC"
+
+else
+
+    tmux has-session -t "master"  > /dev/null 2>&1
+if [[ "$?" == "1" ]]; then
+    tmux new-session -s "master" \; set-environment TOPIC "$TOPIC"
+else
+    sesname=$(echo "${TOPIC}-${SES_NAME}" | sed 's/|/-/')
+    tmux new-session -t "master" -s "${sesname}"  \; new-window \; set-environment TOPIC "$TOPIC"
+fi
+fi
